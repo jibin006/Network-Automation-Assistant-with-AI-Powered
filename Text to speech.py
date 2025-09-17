d5f8cacbbc43e1151f067f9e4d5d9bdb0006d0ce
JIBIN_BENNY <146290857+jibin006@users.noreply.github.com>
2025-09-17 18:33:49 +0530

    Delete Text to speech.py

--- Text to speech.py
+++
@@ -1,8 +0,0 @@
-# Import the required module for text-to-speech conversion
-import pyttsx3
-# Initialize the speech synthesis engine
-engine = pyttsx3.init()
-# Modify the say method to include personalized details
-engine.say('Hello, my name is Javis, How may I assist you today sir?')
-# Run and wait method to process the voice commands
-engine.runAndWait()
